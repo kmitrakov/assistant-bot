994c68de66cd4d115f41d947c8ac0a934177c294
Митраков Кирилл <k.mitrakov@telecontact.ru>
2022-12-30 18:43:13 +0300

    Добавлена команда /weather_moscow. Добавлена команда /random_dog.

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import httplib2
 from settings import *
 from decouple import config
 from datetime import datetime
+from pprint import pprint
 
 telegram_token = config("telegram_token", default="")
+weather_token = config("weather_token", default="")
 
 
 def get_price(currency_pair):
@@ -21,6 +23,40 @@ def get_price(currency_pair):
         sell_price = response[currency_pair]["sell"]
 
         return f"Цена продажи {currency_pair_splitted[0].upper()}: {sell_price} {currency_pair_splitted[1].upper()}"
+
+    except Exception as ex:
+        print(ex)
+        return "Что-то пошло не так..."
+
+
+def get_weather(weather_token):
+    request_url = weather_url + weather_token
+
+    try:
+
+        req = requests.get(request_url, timeout=3)
+        data = req.json()
+
+        weather = data["weather"][0]["description"].capitalize()
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise = datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset = datetime.fromtimestamp(data["sys"]["sunset"])
+        sunrise_time = sunrise.strftime("%H:%M:%S")
+        sunset_time = sunset.strftime("%H:%M:%S")
+        length_of_the_day = sunset - sunrise
+
+        return f"Погода в Москве ({weather}):\n"\
+               f"- Температура: {temperature} C°\n"\
+               f"- Влажность: {humidity}%\n"\
+               f"- Давление: {pressure} мм\n"\
+               f"- Ветер: {wind} м/сек\n"\
+               f"- Восход солнца: {sunrise_time}\n"\
+               f"- Закат солнца: {sunset_time}\n"\
+               f"- Продолжительность дня: {length_of_the_day}"
+
     except Exception as ex:
         print(ex)
         return "Что-то пошло не так..."
@@ -37,6 +73,8 @@ def telegram_bot(telegram_token):
             "/price_btc - показать текущую цену продажи Bitcoin (BTC)\n"
             "/price_eth - показать текущую цену продажи Ethereum (ETH)\n"
             "/random_cat - показать котика\n"
+            "/random_dog - показать пёсика\n"
+            "/weather_moscow - показать погоду в Москве\n"
             "/help - показать список команд"
         )
 
@@ -56,12 +94,12 @@ def telegram_bot(telegram_token):
 
     @bot.message_handler(commands=["random_cat"])
     def random_cat_message(message):
-        url = random_cat_url
+        request_url = random_cat_url
         file_temp = "temp/images/" + datetime.now().strftime("%Y%m%d%H%M%S") + ".jpeg"
 
         try:
             h = httplib2.Http('.cache')
-            response, content = h.request(url)
+            response, content = h.request(request_url)
             out = open(file_temp, 'wb')
             out.write(content)
             out.close()
@@ -72,12 +110,51 @@ def telegram_bot(telegram_token):
                 message.chat.id,
                 image
             )
+
         except Exception as ex:
             print(ex)
             bot.send_message(
                 message.chat.id,
                 "Что-то пошло не так..."
             )
+
+    @bot.message_handler(commands=["random_dog"])
+    def random_dog_message(message):
+        request_url = random_dog_url
+        file_temp = "temp/images/" + datetime.now().strftime("%Y%m%d%H%M%S") + ".jpeg"
+
+        try:
+            req = requests.get(request_url, timeout=3)
+            data = req.json()
+
+            request_url_1 = data[0]
+
+            h = httplib2.Http('.cache')
+            response, content = h.request(request_url_1)
+            out = open(file_temp, 'wb')
+            out.write(content)
+            out.close()
+
+            image = open(file_temp, 'rb')
+
+            bot.send_photo(
+                message.chat.id,
+                image
+            )
+
+        except Exception as ex:
+            print(ex)
+            bot.send_message(
+                message.chat.id,
+                "Что-то пошло не так..."
+            )
+
+    @bot.message_handler(commands=["weather_moscow"])
+    def weather_moscow(message):
+        bot.send_message(
+            message.chat.id,
+            get_weather(weather_token)
+        )
 
     @bot.message_handler(content_types=["text"])
     def send_text(message):
@@ -91,4 +168,4 @@ def telegram_bot(telegram_token):
 
 if __name__ == "__main__":
     telegram_bot(telegram_token)
-
+    # print(get_weather(weather_token))
--- settings.py
+++ settings.py
@@ -1,2 +1,4 @@
 exchange_api_url = "https://yobit.net/api/3/ticker/"
 random_cat_url = "https://cataas.com/cat"
+random_dog_url = "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true"
+weather_url = "https://api.openweathermap.org/data/2.5/weather?lat=55.7504461&lon=37.6174943&units=metric&lang=ru&appid="
